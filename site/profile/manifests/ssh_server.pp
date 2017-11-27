d66f08ae3c57634315c291ede174c8d5df321d42
Gillian Gunson <ggunson@github.com>
2017-11-26 21:12:10 -0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDt9M+iLwcS7By9nduvdTckLNgQ2neqSTOsGI/+EOIiLrcG+SWVSJ75NrzUU0nYm2UCrUUaPNnFeHsmL+ErTW9At24PaC46dYQVLR3IUOl7c3RUAJbKl13RE34PXYD5PE+c+pl+qYxOV8vZv7elrewBMJgvIKOo4tOutXiUbGKGk4H5rRwtsqTeTbj3bDGcGvfq0LFjoEtJ7OzbiJX7t8jTGCT2L4CsG+WlomMeeVi+FL4UYUURCoVatYZCl+UvOE9FlaJPENMEqiyt5UF5Sy9My6mJsczFWAvBFqtM5gL/3ITOTjAHZ+MWHXobI3e4CJeJDDRch+9lSyrDlYDUk/Jd',
 	}
 }
